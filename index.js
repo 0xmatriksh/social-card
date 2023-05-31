a468ec62e8e4a916b2b2add04530ae76c292be93
kishmatbhtr <kishmat@konnectcraft.com>
2023-05-31 23:57:29 +0545

    update2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Get the canvas element
 function createImage(title, description) {
-    var canvas = document.getElementById("customImageCanvas");
+    var canvas = document.createElement("canvas");
+    canvas.id = "customImageCanvas";
     var context = canvas.getContext("2d");
 
     // Set the canvas size (e.g., 600x300 pixels)
@@ -78,6 +79,8 @@ function createImage(title, description) {
     // Load a random image and draw it inside the circle
     var image = new Image();
     image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9XVwgTDJCrNKfN1A4ZIhNs0Slk8MBkf48fA"; // Replace with the source of your random image
+    image.setAttribute("crossorigin", "anonymous")
+
 
     image.onload = function () {
         // Clip the rendering to the circle
@@ -98,17 +101,17 @@ function createImage(title, description) {
         context.restore(); // Restore the context to remove the clipping path
 
         // Convert the canvas to a data URL
+        var dataURL = canvas.toDataURL("image/jpeg");
+        console.log(dataURL)
 
+        // Update the meta tag with the custom image
+        var metaTag = document.querySelector('meta[property="twitter:image"]');
+        metaTag.setAttribute("content", dataURL);
     };
 
-    var dataURL = canvas.toDataURL("image/jpeg");
-    console.log(dataURL)
-
-    // Update the meta tag with the custom image
-    var metaTag = document.querySelector('meta[property="twitter:image"]');
-    metaTag.setAttribute("content", dataURL);
     // // Convert the canvas to a data URL
     // var dataURL = canvas.toDataURL("image/jpeg");
+    // console.log(dataURL);
 
     // // Update the meta tag with the custom image
     // var metaTag = document.querySelector('meta[property="twitter:image"]');
@@ -118,62 +121,62 @@ function createImage(title, description) {
 
 createImage("Investor from Nepal", "Dividend growth investing and how I grow dividend")
 
-// Create the first canvas
-var canvas1 = document.createElement('canvas');
-canvas1.width = 150; // Specify the width of the first canvas
-canvas1.height = 300; // Specify the height of the first canvas
-var ctx1 = canvas1.getContext('2d');
+// // Create the first canvas
+// var canvas1 = document.createElement('canvas');
+// canvas1.width = 150; // Specify the width of the first canvas
+// canvas1.height = 300; // Specify the height of the first canvas
+// var ctx1 = canvas1.getContext('2d');
 
-// Draw on the first canvas (optional)
-ctx1.fillStyle = '#45b559'; // Set the background color of the first canvas
-ctx1.fillRect(0, 0, canvas1.width, canvas1.height);
+// // Draw on the first canvas (optional)
+// ctx1.fillStyle = '#45b559'; // Set the background color of the first canvas
+// ctx1.fillRect(0, 0, canvas1.width, canvas1.height);
 
-// Create the second canvas
-var canvas2 = document.createElement('canvas');
-canvas2.width = 400; // Specify the width of the second canvas
-canvas2.height = 300;// Specify the height of the second canvas
-var ctx2 = canvas2.getContext('2d');
+// // Create the second canvas
+// var canvas2 = document.createElement('canvas');
+// canvas2.width = 400; // Specify the width of the second canvas
+// canvas2.height = 300;// Specify the height of the second canvas
+// var ctx2 = canvas2.getContext('2d');
 
-// Draw on the second canvas (optional)
-ctx2.fillStyle = '#ffffff'; // Set the background color of the second canvas
-ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
+// // Draw on the second canvas (optional)
+// ctx2.fillStyle = '#ffffff'; // Set the background color of the second canvas
+// ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
 
-ctx2.fillStyle = "black"; // Set the title text color to #34eb55
-ctx2.font = "bold 26px Arial"; // Set the title font and size
+// ctx2.fillStyle = "black"; // Set the title text color to #34eb55
+// ctx2.font = "bold 26px Arial"; // Set the title font and size
 
-// Add title text to the canvas
-var titleText = "title"; // Replace with your desired title text
-var titleTextWidth = ctx2.measureText(titleText).width;
-var titleTextX = (canvas2.width - titleTextWidth) / 2; // Center the title text horizontally
-var titleTextY = canvas2.height / 2 - 15; // Position the title text vertically
+// // Add title text to the canvas
+// var titleText = "title"; // Replace with your desired title text
+// var titleTextWidth = ctx2.measureText(titleText).width;
+// var titleTextX = (canvas2.width - titleTextWidth) / 2; // Center the title text horizontally
+// var titleTextY = canvas2.height / 2 - 15; // Position the title text vertically
 
-ctx2.fillText(titleText, titleTextX, titleTextY); // Render the title text
-
-
-// Set the description text properties
-ctx2.fillStyle = "black"; // Set the description text color to #34eb55
-ctx2.font = "italic 14px Arial"; // Set the description font and size
-
-// Add description text to the canvas
-var descriptionText = "description ctx2 ctx2ctx2ctx2ctx2ctx2ctx2ctx2 ctx2ctx2ctx2 Hello Hi Hello"; // Replace with your desired description text
-var descriptionTextWidth = ctx2.measureText(descriptionText).width;
-var descriptionTextX = (canvas2.width - descriptionTextWidth) / 2; // Center the description text horizontally
-var descriptionTextY = canvas2.height / 2 + 15; // Position the description text vertically
-
-ctx2.fillText(descriptionText, descriptionTextX, descriptionTextY); // Render the description text
+// ctx2.fillText(titleText, titleTextX, titleTextY); // Render the title text
 
 
-// Create a larger canvas to merge the two canvas elements
-var mergedCanvas = document.createElement('canvas');
-mergedCanvas.width = canvas1.width + canvas2.width; // Specify the width of the merged canvas
-mergedCanvas.height = Math.max(canvas1.height, canvas2.height); // Specify the height of the merged canvas
-var ctxMerged = mergedCanvas.getContext('2d');
+// // Set the description text properties
+// ctx2.fillStyle = "black"; // Set the description text color to #34eb55
+// ctx2.font = "italic 14px Arial"; // Set the description font and size
 
-// Merge the two canvas elements onto the larger canvas
-ctxMerged.drawImage(canvas1, 0, 0);
-ctxMerged.drawImage(canvas2, canvas1.width, 0);
+// // Add description text to the canvas
+// var descriptionText = "description ctx2 ctx2ctx2ctx2ctx2ctx2ctx2ctx2 ctx2ctx2ctx2 Hello Hi Hello"; // Replace with your desired description text
+// var descriptionTextWidth = ctx2.measureText(descriptionText).width;
+// var descriptionTextX = (canvas2.width - descriptionTextWidth) / 2; // Center the description text horizontally
+// var descriptionTextY = canvas2.height / 2 + 15; // Position the description text vertically
 
-// Use the merged canvas as needed
-// For example, you can append it to the document body:
-document.body.appendChild(mergedCanvas);
+// ctx2.fillText(descriptionText, descriptionTextX, descriptionTextY); // Render the description text
+
+
+// // Create a larger canvas to merge the two canvas elements
+// var mergedCanvas = document.createElement('canvas');
+// mergedCanvas.width = canvas1.width + canvas2.width; // Specify the width of the merged canvas
+// mergedCanvas.height = Math.max(canvas1.height, canvas2.height); // Specify the height of the merged canvas
+// var ctxMerged = mergedCanvas.getContext('2d');
+
+// // Merge the two canvas elements onto the larger canvas
+// ctxMerged.drawImage(canvas1, 0, 0);
+// ctxMerged.drawImage(canvas2, canvas1.width, 0);
+
+// // Use the merged canvas as needed
+// // For example, you can append it to the document body:
+// document.body.appendChild(mergedCanvas);
 
